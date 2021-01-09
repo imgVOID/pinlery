2d83a938ed8fd6772b16572fe1db7152d8e835ac
imgVOID <imgvoid@gmail.com>
2021-01-10 00:55:55 +0300

    fancybox and isotope grid fixes

--- gallery/static/gallery/js/lightbox_image_link.js
+++
@@ -1,8 +0,0 @@
-    $(document).on('afterShow.fb', function( e, instance, slide ) {
-        $(".fancybox-image").wrap('<a href="' + slide.opts.link + '" />');
-        document.body.style.cursor = 'grab';
-    });
-
-    $(document).on('afterClose.fb', function( e, instance, slide ) {
-        document.body.style.cursor = 'default';
-    });
